f332f4a86f5e2ac3c72c651c82f8ec8634aa6ac6
Richard Easton <richeaston@users.noreply.github.com>
2018-04-25 20:46:44 +0100

    Stored feeds locally
    file_get_content of rss and stores it in feeds/[name].xml
    Also now "explodes" the feed entry which consists of [name],[feed_url]

--- index.php
+++ index.php
@@ -4,19 +4,25 @@
 			$handle = fopen("feeds.txt", "r");
 			if ($handle) {
 				while (($line = fgets($handle)) !== false) {
-				$line = preg_replace('/[\r\n]+/','', $line)
+				$feedline = preg_replace('/[\r\n]+/','', $line);
+				$line = explode(",",$feedline);
+				$path = "feeds/" . $line[0] . ".xml";
+				
+				$xml = file_get_contents($line[1]);
+				file_put_contents($path, $xml);
 				// process the line read.
 				?>
 				<div class="col-md-4">
 					<div class="card vulncard divider-outside-bottom">
 						<div class="card-block">
 							<h5 class="card-title vulntitlesub">
-								<?php echo $line ?>
+								<?php echo $line[0] ;?> 
 							</h5>
 							<p class="card-text">
 							<?php
 								$rss = new DOMDocument();
-								$rss->load($line);
+								//$rss->load($line[1]);
+								$rss->load($path);
 								$feed = array();
 								foreach ($rss->getElementsByTagName('item') as $node) {
 									$item = array ( 
